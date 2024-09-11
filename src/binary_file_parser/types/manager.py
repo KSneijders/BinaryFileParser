5c8c4a8e76785174eb9cfcce3b9c5b08ef57fbbf
KSneijders <ksneijders-dev@hotmail.com>
2024-09-11 22:39:53 +0200

    Added Manager __str__ implementation

--- src/binary_file_parser/types/manager.py
+++ src/binary_file_parser/types/manager.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import Any, TYPE_CHECKING
 
 if TYPE_CHECKING:
     from binary_file_parser.retrievers import Retriever, RetrieverCombiner, RetrieverRef
@@ -32,4 +32,42 @@ class Manager:
     def __init__(self, struct: BaseStruct):
         self._struct = struct
 
-    # todo: pretty print and degug stuff. same as base struct. perhaps create a superclass.
+    def __str__(self):
+        strings = [f"\n{self.__class__.__name__}:"]
+
+        attributes = [attr for attr in dir(self) if not attr.startswith('_')]
+
+        table = {}
+        for attr in attributes:
+            value = Manager._format_value(getattr(self, attr))
+            table[attr] = value
+
+        longest_key = 0
+        for key in table:
+            if len(key) > longest_key:
+                longest_key = len(key)
+
+        for key, value in table.items():
+            strings.append(f"\t{key.ljust(longest_key)} = {value}")
+
+        return '\n'.join(strings)
+
+    @staticmethod
+    def _format_value(value: Any) -> str:
+        if type(value) is str:
+            return f"'{value}'"
+        if type(value) is bytes:
+            return f"b'{value.hex()}'"
+        if isinstance(value, list) and len(value) > 0:
+            item_string = Manager._format_value(value[0])
+            if len(value) > 1:
+                item_string += ', ' + Manager._format_value(value[1])
+            if len(value) > 2:
+                item_string += ', + ' + str(len(value) - 2) + ' more'
+
+            return f"[{item_string}]"
+        # Type check (with inheritance without needing import) ThxSO
+        if 'BaseStruct' in [t.__name__ for t in type(value).__mro__]:
+            return value.__class__.__name__
+
+        return str(value)
